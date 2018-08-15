ce99118a8eca3b2207f99b4b6165f41d16c07183
Idrizovich <dino.i@hotmail.com>
2018-08-15 02:00:56 +0200

    delete + edit

--- files/controllers/main-controller.js
+++ files/controllers/main-controller.js
@@ -1,5 +1,12 @@
 function MainController($scope, $rootScope, $http, toastr){
-    $scope.openModal = function () {
+    
+  function getEvent() {
+    $http.get('/events').then(function (response) {
+      $scope.events = response.data
+    })
+  }
+  
+  $scope.openModal = function () {
         $scope.visible = false;
         $scope.visible = $scope.visible = true;
       }
@@ -10,15 +17,43 @@ function MainController($scope, $rootScope, $http, toastr){
       } 
 
       $scope.addEvent = function () {
-        console.log('add event')
-        console.log($scope.events)
-        $http.post('/event', $scope.events).then(function (response) {
-          console.log(response)
-          $scope.events.eventBandName = ''
-          $scope.events.eventTime = ''
-          $scope.events.eventLocalName = ''
-          $scope.events.eventLocalInfo = ''
-          toastr.success("Event added successfully")
+        $http.post('/event', $scope.event).then(function (response) {
+          toastr.success("Event added successfully");
+          getEvent();
         })
       }
+
+      $scope.deleteEvent = function(id){
+        $http.delete('/events/'+ id).then(function(data){
+          getEvent()
+            toastr.success("You have successfully deleted an event!");
+        });
+    }
+
+  /*   $scope.updateEvent= function(){
+      $http.put('/events/'+$scope.events._id, $scope.event).then(function(data){
+        getEvent();
+        toastr.info('You have successfully updated band!');
+        $scope.event = null;
+      });
+    }    
+    $scope.editEvent = function(event){
+      $scope.event ={
+        _id : event._id,
+        eventBandName : event.eventBandName,
+        eventLocalName : event.eventLocalName,
+        eventLocalInfo : event.eventLocalInfo,
+        eventTime : event.eventTime,
+      };
+  }
+ */
+$scope.editEvent = function(event){
+  $http.put('/events', event).then(function(response){
+    getEvent();
+    toastr.info('You have successfully updated band!');
+  }, function(error){
+    console.log(error);
+  });
+}
+      getEvent()
 }
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const jwt_secret = 'WU5CjF8fHxG40S2t7oyk';
@@ -8,14 +8,11 @@ var db = mongojs('localhost:27017/Bandz', ['events'])
 
 var jwt = require('jsonwebtoken');
 var MongoId = require('mongodb').ObjectID;
-var db;
 
-app.use(bodyparser.urlencoded({ extended: false }));
-app.use(bodyparser.json());
-
-var urlencodedParser = bodyparser.urlencoded({
-  extended: false
-})
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use('/', express.static('files'));
 
@@ -103,16 +100,64 @@ app.post('/register/band', function (request, response) {
   });
 });
 
-app.post('/event', urlencodedParser, function (request, response, next) {
-  console.log(request.body)
-
-  db.collection('events').save(request.body, (err, result) => {
+app.post('/event', function (request, response) {
+  var event = request.body
+  db.collection('events').insert(event, function(err, result) {
     if (err) return console.log(err);
-    response.send(true);
+    response.setHeader('Content-Type', 'application/json');
+    response.send(event);
   })
 })
 
-MongoClient.connect('mongodb://localhost:27017/Bandz', (err, database) => {
+app.get('/events', function (req, res) {
+  db.collection('events').find().toArray((err, event) => {
+    if (err) return console.log(err);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(event);
+  })
+});
+
+app.delete('/events/:event_id', function(req, res){
+  db.collection('events').remove({_id: new MongoId(req.params.event_id)},
+  function(err, data){
+      res.json(data);
+  });
+});
+
+app.put('/events', function(req, res){
+  event = req.body;
+  db.collection('events').findOneAndUpdate( {_id: new MongoId(event._id) }, {
+    $set : {eventBandName: req.body.eventBandName,
+      eventTime: req.body.eventTime,
+      eventLocalName: req.body.eventLocalName,
+      eventLocalInfo: req.body.eventLocalInfo}
+  }, (err, result) => {
+    if (err) return res.send(err);
+    res.send('OK');
+  })
+});
+
+/* 
+app.put('/events/:events_id', function(req, res){    
+  db.collection('events').findAndModify(
+     {_id: new MongoId(req.params.event_id)}, // query
+     [['_id','asc']],  // sort order
+     {$set : {eventBandName: req.body.eventBandName,
+      eventTime: req.body.eventTime,
+      eventLocalName: req.body.eventLocalName,
+      eventLocalInfo: req.body.eventLocalInfo}}, // replacement, replaces only the field "hi"
+     function(err, data) {
+         if (err){
+             console.warn(err.message);  // returns error if no matching object found
+         }else{
+             res.json(data);
+         }
+     });
+}); */
+
+
+
+  MongoClient.connect('mongodb://localhost:27017/Bandz', (err, database) => {
   if (err) return console.log(err)
   db = database;
   app.listen(3000, () => console.log('Example app listening on port 3000!'))
